79cba86983d90c58135ef1baa25119b42c750dd6
Ricc <richard10hahn@gmail.com>
2026-02-21 15:50:51 +0000

    Added formatter

--- formatter/main.js
+++ formatter/main.js
@@ -2,21 +2,41 @@ const params = new URLSearchParams(window.location.search);
 let ts = params.get("ts");
 const result = document.getElementById("result");
 const mode = document.getElementById("select");
+const copyBtn = document.getElementById("copyBtn");
 (async () => {
   if (!ts || isNaN(ts)) {
     const input = document.getElementById("ts");
-    input.style.visibility = "";
+    input.style.display = "";
     ts = await new Promise((res, rej) => {
-        input.addEventListener("change", (ev) => {
-            res(input.value);
-            input.style.visibility = "hidden";
-        })
+      input.addEventListener("change", (ev) => {
+        res(input.value);
+        input.style.display = "none";
+      });
     });
-
-    result.textContent = format(ts, mode.value);
   }
+  copyBtn.disabled = false;
+  result.textContent = format(ts, mode.value);
 })();
+
+
+mode.addEventListener("change", () => {
+  result.textContent = format(ts, mode.value);
+})
+
+copyBtn.addEventListener("click", () => {
+  copy(copyBtn)
+})
 
 function format(int, mode) {
   return `<t:${int}:${mode}>`;
+}
+
+function copy(btn) {
+  const txt = btn.parentElement.querySelector('div').innerText;
+  navigator.clipboard.writeText(txt).then(() => {
+    btn.textContent = 'Copied!'
+    setTimeout(() => {
+      btn.textContent = 'Copy'
+    }, 1000*3)
+  })
 }
--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-
 function time() {
-  const inMs = document.getElementById('mode').checked
-  const txt = document.getElementById('result')
-  let timeEl = document.getElementById('time');
-  const copyBtn = document.getElementById('copyBtn');
-  if (!timeEl.value) { 
-    copyBtn.disabled = true
-    copyBtn.style.background = 'lightgray'
-    txt.textContent = "Select a date."
-    timeEl.style.border = '2px solid red'
-    timeEl.style.borderRadius = "2px"
+  const inMs = document.getElementById("mode").checked;
+  const txt = document.getElementById("result");
+  let timeEl = document.getElementById("time");
+  const copyBtn = document.getElementById("copyBtn");
+  const link = document.getElementById("link");
+  if (!timeEl.value) {
+    copyBtn.disabled = true;
+    copyBtn.style.background = "lightgray";
+    txt.textContent = "Select a date.";
+    timeEl.style.border = "2px solid red";
+    timeEl.style.borderRadius = "2px";
     setTimeout(() => {
-      timeEl.style.border = "1px solid grey"
-    }, 1000 * 3)
+      timeEl.style.border = "1px solid grey";
+    }, 1000 * 3);
     return;
   }
-  copyBtn.disabled = false
-  copyBtn.style.background = 'darkgray'
+  copyBtn.disabled = false;
+  copyBtn.style.background = "darkgray";
   timeEl = timeEl.value;
-  const [datePt, timePt = '00:00'] = timeEl.split('T')
-  const [y, m, d] = datePt.split('-').map(Number)
-  const [hh, mm] = timePt.split(':').map(v => Number(v))
-  const date = new Date(y, m - 1, d, hh, mm, 0, 0).getTime()
+  const [datePt, timePt = "00:00"] = timeEl.split("T");
+  const [y, m, d] = datePt.split("-").map(Number);
+  const [hh, mm] = timePt.split(":").map((v) => Number(v));
+  const date = new Date(y, m - 1, d, hh, mm, 0, 0).getTime();
   if (inMs) {
     txt.textContent = date;
+    link.style.display = "";
+    link.href = "./formatter/?ts=" + date;
     return;
   }
-  const s = Math.floor(date / 1000)
-  txt.textContent = s
+  const s = Math.floor(date / 1000);
+  txt.textContent = s;
+  link.style.display = "";
+  link.href = "./formatter/?ts=" + s;
 }
 function copy(btn) {
-  const txt = btn.parentElement.querySelector('div').innerText;
+  const txt = btn.parentElement.querySelector("div").innerText;
   navigator.clipboard.writeText(txt).then(() => {
-    btn.textContent = 'Copied!'
+    btn.textContent = "Copied!";
     setTimeout(() => {
-      btn.textContent = 'Copy'
-    }, 1000*3)
-  })
+      btn.textContent = "Copy";
+    }, 1000 * 3);
+  });
 }
